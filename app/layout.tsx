0c68c951f9149165c6fa0a50fbdbd900b1332f96
gangbeomsu <kbs21c1987@gmail.com>
2023-12-25 18:00:15 +0900

    로그인 인증정보 가져오기

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import NavBar from '@/components/nav/NavBar';
+import SessionProvider from '@/components/session-provider';
 import { ThemeProvider } from '@/components/theme-provider';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
@@ -30,6 +31,7 @@ export default function RootLayout({
             {children}
           </main>
         </ThemeProvider>
+        <SessionProvider />
       </body>
     </html>
   );
--- components/nav/NavBar.tsx
+++ components/nav/NavBar.tsx
@@ -1,7 +1,12 @@
+'use client';
+
+import { useUser } from '@/lib/store/user';
 import Link from 'next/link';
 import LoginForm from './LoginForm';
 
 export default function NavBar() {
+  const user = useUser((state) => state.user);
+
   return (
     <nav className="flex items-center justify-between">
       <div className="group">
@@ -10,7 +15,8 @@ export default function NavBar() {
         </Link>
         <div className="h-1 w-0 group-hover:w-full transition-all bg-green-500"></div>
       </div>
-      <LoginForm />
+
+      {user?.id ? <h1>Profile</h1> : <LoginForm />}
     </nav>
   );
 }
